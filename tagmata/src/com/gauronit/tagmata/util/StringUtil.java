db468878ede733ac086af2c8f11c4d46edd83a05
jujadhav <jujadhav@gmail.com>
2012-10-12 17:45:41 +0530

    Signed-off-by: jujadhav <jujadhav@gmail.com>

--- tagmata/src/com/gauronit/tagmata/util/StringUtil.java
+++ tagmata/src/com/gauronit/tagmata/util/StringUtil.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Gauronit Technologies.
+ * All rights reserved. Tagmata and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Jayesh Jadhav - initial API and implementation
+ ******************************************************************************/
 package com.gauronit.tagmata.util;
 
 import java.io.FileWriter;
