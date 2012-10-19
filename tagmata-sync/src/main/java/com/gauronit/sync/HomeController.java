a70cf0ca814e9c7cac13de95a9c4d4323b7ae437
jujadhav <jujadhav@gmail.com>
2012-10-19 17:28:16 +0530

    Initial Upload
    Signed-off-by: jujadhav <jujadhav@gmail.com>

--- tagmata-sync/src/main/java/com/gauronit/sync/HomeController.java
+++ tagmata-sync/src/main/java/com/gauronit/sync/HomeController.java
@@ -1,14 +1,12 @@
 /*******************************************************************************
- * /*******************************************************************************
- *  * Copyright (c) 2012 Gauronit Technologies.
- *  * All rights reserved. Tagmata and the accompanying materials
- *  * are made available under the terms of the GNU Public License v3.0
- *  * which accompanies this distribution, and is available at
- *  * http://www.gnu.org/licenses/gpl.html
- *  * 
- *  * Contributors:
- *  *     Jayesh Jadhav - initial API and implementation
- *  ******************************************************************************/
+ * Copyright (c) 2012, Gauronit Technologies.
+ * All rights reserved. Tagmata, Tagmata Sync and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Jayesh Jadhav - initial API and implementation
  ******************************************************************************/
 package com.gauronit.sync;
 
