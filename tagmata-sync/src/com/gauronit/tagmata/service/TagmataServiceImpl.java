3d7aa49997642d4fbf8db3f593ca0f60dde39b6c
jujadhav <jujadhav@gmail.com>
2012-10-23 09:40:18 +0530

    Initial Upload
    Signed-off-by: jujadhav <jujadhav@gmail.com>

---
+++ tagmata-sync/src/com/gauronit/tagmata/service/TagmataServiceImpl.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2012, Gauronit, LLC.
+ * All rights reserved. Tagmata and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Jayesh J - initial API and implementation
+ ******************************************************************************/
+package com.gauronit.tagmata.service;
+
+import com.gauronit.tagmata.schema.FetchCardsResponse;
+import com.gauronit.tagmata.schema.RemoveSyncCardsResponse;
+import com.gauronit.tagmata.schema.SyncCardsResponse;
+import com.gauronit.tagmata.schema.TagmataException;
+
+public class TagmataServiceImpl implements TagmataService {
+
+	public void initialize() {
+		
+	}
+	
+	@Override
+	public FetchCardsResponse fetchCardsRequest() throws TagmataException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SyncCardsResponse syncCardsRequest() throws TagmataException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RemoveSyncCardsResponse removeSyncCardsResponse()
+			throws TagmataException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
